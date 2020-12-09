928b0795831ed3020920aab5aa1549e296ea0b61
Ben Gosney <bengosney@googlemail.com>
2020-12-09 08:44:15 +0000

    Day 9

---
+++ day-9.py
@@ -0,0 +1,39 @@
+import utils
+
+input = [int(i) for i in utils.getInput(9).split("\n")]
+inputLength = len(input)
+
+preableSize = 25
+part1 = None
+part2 = None
+for i in range(preableSize, inputLength):
+    found = False
+    for j in range(i - preableSize, i):
+        for k in range(i - preableSize, i):
+            if input[j] != input[k] and (input[j] + input[k]) == input[i]:
+                found = True
+
+    if not found:
+        part1 = input[i]
+        break
+
+print(f"part 1: {part1}")
+
+for i in range(inputLength):
+    for j in range(inputLength):
+        if j - i < 2:
+            continue
+
+        currentRange = input[i:j]
+        rangeSum = sum(currentRange)
+        if rangeSum == part1:
+            part2 = min(currentRange) + max(currentRange)
+            break
+
+        if rangeSum > part1:
+            break
+
+    if part2 is not None:
+        break
+
+print(f"part 2: {part2}")
