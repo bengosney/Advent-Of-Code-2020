435bf8df32a2954beb74606702f9bac7eca2d650
Ben Gosney <bengosney@googlemail.com>
2020-12-11 13:53:44 +0000

    Day 11 - part 2 - the code is not very nice

--- day-11.py
+++ day-11.py
@@ -2,32 +2,36 @@ import utils
 
 input = utils.getInput(11)
 
-
 currentState = []
 for row in input.splitlines():
     r = []
     for col in row:
         r.append(col)
     currentState.append(r)
+
+orgState = [r[:] for r in currentState]
+
 rowCount = len(currentState)
 colCount = len(currentState[0])
 
-# for row in input:
-#     for col in row:
-#         state.append([col.strip()])
 
-
-def occupied(state, x, y):
+def occupied(state, x, y, inview):
     c = 0
     for mx in [-1, 0, 1]:
         for my in [-1, 0, 1]:
-            _x = x + mx
-            _y = y + my
-            if _x == x and _y == y:
-                continue
-            if _x < 0 or _y < 0:
-                continue
             try:
+                _x = x + mx
+                _y = y + my
+
+                if _x == x and _y == y:
+                    continue
+
+                while inview and state[_x][_y] == ".":
+                    _x += mx
+                    _y += my
+
+                if _x < 0 or _y < 0:
+                    continue
                 if state[_x][_y] == "#":
                     c += 1
             except IndexError:
@@ -35,16 +39,16 @@ def occupied(state, x, y):
     return c
 
 
-def doFrame(state):
+def doFrame(state, tolerance, inview):
     newState = [r[:] for r in state]
     for x, row in enumerate(state):
         for y, col in enumerate(row):
-            occ = occupied(state, x, y)
+            occ = occupied(state, x, y, inview)
             if col == "L":
                 if occ == 0:
                     newState[x][y] = "#"
             elif col == "#":
-                if occ >= 4:
+                if occ >= tolerance:
                     newState[x][y] = "L"
 
     return newState
@@ -57,8 +61,7 @@ def draw(state):
 
 
 while True:
-    # draw(currentState)
-    nextState = doFrame(currentState)
+    nextState = doFrame(currentState, 4, False)
     if nextState == currentState:
         break
     currentState = nextState
@@ -70,3 +73,18 @@ for i in currentState:
             seatsTaken += 1
 
 print(f"part 1: {seatsTaken}")
+
+currentState = [r[:] for r in orgState]
+while True:
+    nextState = doFrame(currentState, 5, True)
+    if nextState == currentState:
+        break
+    currentState = nextState
+
+seatsTaken = 0
+for i in currentState:
+    for j in i:
+        if j == "#":
+            seatsTaken += 1
+
+print(f"part 2: {seatsTaken}")
