db7c0b95f7bc7c4d34f67c8f4b1e53e0163ad2e1
Ben Gosney <bengosney@googlemail.com>
2020-12-13 10:50:46 +0000

    Day 13

---
+++ day-13.py
@@ -0,0 +1,42 @@
+import math
+
+import utils
+
+input = utils.getInput(13).splitlines()
+
+start = int(input[0])
+
+
+class Bus:
+    def __init__(self, id, position) -> None:
+        super().__init__()
+        self.id = int(id)
+        self.position = int(position)
+        self.last = None
+
+    def __str__(self) -> str:
+        return f"{self.id}"
+
+    def next(self, after: int):
+        return self.id * math.ceil(after / self.id)
+
+
+busses = [Bus(i, p) for p, i in enumerate(input[1].split(","), start=0) if i != "x"]
+
+min = None
+currentBestBus = None
+
+for bus in busses:
+    ts = bus.next(start)
+    if min is None or ts < min:
+        min = ts
+        currentBestBus = bus
+
+print(f"part 1: {currentBestBus.id * (min - start)}")
+
+invm = lambda a, b: 0 if a == 0 else 1 if b % a == 0 else b - invm(b % a, a) * b // a
+
+N = math.prod([b.id for b in busses])
+x = sum([b.position * (N // b.id) * invm(N // b.id, b.id) for b in busses])
+
+print(f"part 2: {N - x % N}")
--- utils.py
+++ utils.py
@@ -2,5 +2,5 @@ import os
 
 
 def getInput(day: int) -> list:
-    with open(os.path.join("inputs", f"day-{day}.txt")) as f:
+    with open(os.path.join(os.path.dirname(__file__), "inputs", f"day-{day}.txt")) as f:
         return f.read().strip()
