9d2de56c7d030d817a30a33b42efed1ce547dfab
Ben Gosney <bengosney@googlemail.com>
2020-12-19 11:24:37 +0000

    Day 19 - part 2 - not nice

--- day-19.py
+++ day-19.py
@@ -14,17 +14,18 @@ class Rules:
             id, details = rule.split(":")
             self.rules[int(id)] = details.strip().replace('"', "")
 
-    def compile(self, id=0):
+    def compile(self, id=0, seen=[]):
         regexParts = []
         rule = self.rules[id]
-        for p in rule.split("|"):
-            ruleRegex = ""
-            for r in p.split(" "):
-                if r.isnumeric():
-                    ruleRegex += self.compile(int(r))
-                else:
-                    ruleRegex += r
-            regexParts.append(ruleRegex)
+        if seen.count(id) < 10:
+            for p in rule.split("|"):
+                ruleRegex = ""
+                for r in p.split(" "):
+                    if r.isnumeric():
+                        ruleRegex += self.compile(int(r), seen[:] + [id])
+                    else:
+                        ruleRegex += r
+                regexParts.append(ruleRegex)
 
         regex = "|".join(regexParts)
 
@@ -32,12 +33,18 @@ class Rules:
             regex = f"^{regex}$"
 
         if len(regexParts) > 1:
-            return f"({regex})"
+            return f"(?:{regex})"
         else:
             return regex
 
 
 rules = Rules(rawRules)
-matches = [f"{m}" for m in re.finditer(rules.compile(), inputLines, re.MULTILINE)]
 
-print(f"{len(matches)}")
+matches = [f"{m}" for m in re.finditer(rules.compile(), inputLines, re.MULTILINE)]
+print(f"part 1: {len(matches)}")
+
+rules.rules[8] = "42 | 42 8"
+rules.rules[11] = "42 31 | 42 11 31"
+
+matches = [f"{m}" for m in re.finditer(rules.compile(), inputLines, re.MULTILINE)]
+print(f"part 2: {len(matches)}")
