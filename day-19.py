77552ac69c3a24afece257cdf64e4e849fa52618
Ben Gosney <bengosney@googlemail.com>
2020-12-19 11:35:27 +0000

    it now at least works out the recursions needed

--- day-19.py
+++ day-19.py
@@ -14,15 +14,18 @@ class Rules:
             id, details = rule.split(":")
             self.rules[int(id)] = details.strip().replace('"', "")
 
-    def compile(self, id=0, seen=[]):
+    def compile(self, maxRecursion):
+        return self._compile(0, [], maxRecursion)
+
+    def _compile(self, id, seen, maxRecursion):
         regexParts = []
         rule = self.rules[id]
-        if seen.count(id) < 10:
+        if seen.count(id) <= maxRecursion:
             for p in rule.split("|"):
                 ruleRegex = ""
                 for r in p.split(" "):
                     if r.isnumeric():
-                        ruleRegex += self.compile(int(r), seen[:] + [id])
+                        ruleRegex += self._compile(int(r), seen[:] + [id], maxRecursion)
                     else:
                         ruleRegex += r
                 regexParts.append(ruleRegex)
@@ -37,14 +40,29 @@ class Rules:
         else:
             return regex
 
+    def _getMatching(self, inputLines, maxRecursion):
+        regex = self.compile(maxRecursion)
+        return [f"{m}" for m in re.finditer(regex, inputLines, re.MULTILINE)]
+
+    def getMatching(self, inputLines):
+        maxRecursion = 0
+        preResults = []
+        curResults = self._getMatching(inputLines, maxRecursion)
+        while preResults != curResults:
+            preResults = curResults
+            maxRecursion += 1
+            curResults = self._getMatching(inputLines, maxRecursion)
+
+        return preResults
+
 
 rules = Rules(rawRules)
 
-matches = [f"{m}" for m in re.finditer(rules.compile(), inputLines, re.MULTILINE)]
+matches = rules.getMatching(inputLines)
 print(f"part 1: {len(matches)}")
 
 rules.rules[8] = "42 | 42 8"
 rules.rules[11] = "42 31 | 42 11 31"
 
-matches = [f"{m}" for m in re.finditer(rules.compile(), inputLines, re.MULTILINE)]
+matches = rules.getMatching(inputLines)
 print(f"part 2: {len(matches)}")
