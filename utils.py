6d7439c6ce486c6cf51db28c0d9f03df59a81a8e
Ben Gosney <bengosney@googlemail.com>
2020-12-20 21:18:19 +0000

    Fix typing

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os
 
 
-def getInput(day: int) -> list:
+def getInput(day: int) -> str:
     with open(os.path.join(os.path.dirname(__file__), "inputs", f"day-{day}.txt")) as f:
         return f.read().strip()
