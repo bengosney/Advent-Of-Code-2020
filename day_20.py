9fd0a649f304bc216a4897f4bb1d1d44afaf8085
Ben Gosney <bengosney@googlemail.com>
2020-12-20 12:14:58 +0000

    I've got a grid

--- day_20.py
+++ day_20.py
@@ -119,36 +119,75 @@ for rawTile in rawTiles:
     tile = Tile.parse(rawTile)
     tiles.append(tile)
 
-tiles[0].draw()
-print()
-tiles[1].draw()
 
-ids = []
-for t1 in tiles:
-    fits = []
-    for t2 in tiles:
-        if t1.id != t2.id:
-            fits += t1.fits(t2)
-    if len(fits) == 2:
-        ids.append(t1.id)
-    else:
-        bob = 1
+def part1():
+    ids = []
+    for t1 in tiles:
+        fits = []
+        for t2 in tiles:
+            if t1.id != t2.id:
+                fits += t1.fits(t2)
+        if len(fits) == 2:
+            ids.append(t1.id)
 
-part1 = 1
-for id in ids:
-    part1 *= id
-print(f"part 1: {part1}")
-exit(0)
+    part1 = 1
+    for id in ids:
+        part1 *= id
 
+    return part1
+
+
+# print(f"part 1: {part1()}")
+
+validPairs = [
+    ("left", "right", False),
+    ("right", "left", False),
+    ("top", "bottom", False),
+    ("bottom", "top", False),
+]
+
+placedIDs = []
 grid = {}
-current = None
-for tile in tiles:
-    if len(grid) == 0:
-        grid[(0, 0)] = tile
-        current = tile
-        continue
+while len(placedIDs) < len(tiles):
+    print(len(placedIDs), len(tiles))
+    for tile in tiles:
+        if tile.id in placedIDs:
+            continue
+        if len(grid) == 0:
+            grid[(0, 0)] = tile
+            placedIDs.append(tile.id)
+            continue
 
-    for x, y in grid:
-        fits = grid[(x, y)].fits(tile)
-        if len(fits) > 0:
-            pass
+        for x, y in grid:
+            fits = grid[(x, y)].fits(tile)
+            if len(fits) > 0:
+                correctFit = None
+                placedIDs.append(tile.id)
+                i = 0
+                while correctFit is None:
+                    fit = grid[(x, y)].fits(tile)[0]
+                    if fit not in validPairs:
+                        tile.rotate()
+                    else:
+                        correctFit = fit
+                        break
+
+                    i += 1
+                    if i == 4:
+                        tile.flipX()
+                    if i == 8:
+                        tile.flipY()
+                    if i == 12:
+                        tile.flipX()
+
+                if correctFit[0] == "top":
+                    grid[(x, y + 1)] = tile
+                if correctFit[0] == "right":
+                    grid[(x + 1, y)] = tile
+                if correctFit[0] == "bottom":
+                    grid[(x, y - 1)] = tile
+                if correctFit[0] == "left":
+                    grid[(x - 1, y)] = tile
+                break
+
+print(len(grid), len(tiles))
