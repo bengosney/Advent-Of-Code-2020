9fddc6cf16dad27cb8d4f6c60653e432fb3b0d6c
Ben Gosney <bengosney@googlemail.com>
2020-12-20 21:19:19 +0000

    Better handle number of runs

--- benchmark.py
+++ benchmark.py
@@ -8,14 +8,15 @@ def benchmark(mod):
     importlib.reload(mod)
 
 
+number = 10
 times = {}
 for name in os.listdir("."):
     if name.startswith("day_"):
         name = name.replace(".py", "")
         print(f"Benchmarking {name}")
         mod = importlib.import_module(name)
-        times[name] = timeit.timeit("benchmark(mod)", globals=locals(), number=1)
+        times[name] = timeit.timeit("benchmark(mod)", globals=locals(), number=number)
 
 print("=" * 80)
 for k, v in sorted(times.items()):
-    print(k, v)
+    print(k, v / number)
