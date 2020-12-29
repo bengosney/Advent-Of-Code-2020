462d3700b76300ca4bad80bb093a3bccfcccd67e
Ben Gosney <bengosney@googlemail.com>
2020-12-29 21:24:05 +0000

    I just found combinations, much smaller day 1

--- day_01.py
+++ day_01.py
@@ -1,23 +1,12 @@
+from itertools import combinations
+from math import prod
+
 import utils
 
 input = [int(i) for i in utils.getInput(1).splitlines()]
 
-p1 = None
-p2 = None
-
-for f in input:
-    for s in input:
-        if (f + s) == 2020 and (f != s):
-            p1 = f * s
-
-        for t in input:
-            if (f + s + t) == 2020 and f != s and f != t and s != t:
-                p2 = f * s * t
-                break
-        if p1 and p2:
+for i in (1, 2):
+    for combo in combinations(input, 1 + i):
+        if sum(combo) == 2020:
+            print(f"part {i}: {prod(combo)}")
             break
-    if p1 and p2:
-        break
-
-print(f"Part 1: {p1}")
-print(f"Part 2: {p2}")
